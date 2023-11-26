a00d6fd2c835533e7aa904a32be83e24590f6ddf
DanielDuarte <danielduarte123004@gmail.com>
2023-11-26 23:22:00 +0000

    Tested and Updated Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final double ACESO = 0.99999999;
         final double APAGADO = 0.0;
 
-        Neuron neuron1 = new Neuron(Arrays.asList(4), 0.1f);
-        Neuron neuron2 = new Neuron(Arrays.asList(4), 1f);
-        Neuron neuron3 = new Neuron(Arrays.asList(4), 0.1f);
-        Neuron neuronF = new Neuron(null, 1f);
+        NeuralNetwork neuralN = null;
+        neuralN =  NeuralNetwork.fromFile(Path.of("network_sample.txt"));
+        double value = neuralN.compute(List.of(APAGADO,ACESO,APAGADO));
+        System.out.println(value);
 
-        neuron1.addInput(ACESO);
-        Double value1 = neuron1.operation();
-        System.out.println(value1);
-        neuronF.addInput(value1);
-
-        neuron2.addInput(APAGADO);
-        Double value2 = neuron2.operation();
-        System.out.println(value2);
-        neuronF.addInput(value2);
-
-        neuron3.addInput(ACESO);
-        Double value3 = neuron3.operation();
-        System.out.println(value3);
-        neuronF.addInput(value3);
-
-        System.out.println(neuronF.operation());
-        System.out.println(neuronF.operation() > 0.5 ? "Apenas o do meio" : "outros");
-
-        // neuroList.forEach(System.out::println);
     }
 }
